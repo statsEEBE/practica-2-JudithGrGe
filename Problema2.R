90063308bc5a99e6bc18688fc53196c203a3d4ed
judith.grau <judith.grau@auleseebe.upc.edu>
2024-10-03 11:06:08 +0200

    Resuelto problema 2

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,37 @@
 #Codigo para problema 2
+
+mis_dades <- iris
+mis_dades
+
+x <- mis_dades$Petal.Length
+y <- mis_dades$Sepal.Length
+
+plot(x,y)
+
+x_bar <- mean(x)
+y_bar <- mean(y)
+
+m <- sum((x-x_bar)*(y-y_bar))/sum((x-x_bar)^2)
+b <- y_bar - m*x_bar
+
+pred1.5 <- m*1.5+b
+x_pred <- x
+y_pred <- m*x_pred+b
+
+plot(x,y)
+lines(x_pred,y_pred)
+
+# Coeficiente de determinación
+Rsq <- sum((y_pred-y_bar)^2)/sum((y-y_bar)^2)
+
+# Coef. de correlación
+R <- sqrt(Rsq)
+cor.test(x,y)
+
+
+# Alternativamente,
+
+mod <- lm(y~x)
+summary(mod)
+
+y_pred2 <- predict(mod, data.frame(x=x))
